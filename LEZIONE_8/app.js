7d19aaf6a851b782cc6b9dda211ccbbedd3c47da
Simona Tocci <simo.tocci@gmail.com>
2023-02-10 13:37:40 +0100

    Update files

--- LEZIONE_8/app.js
+++ LEZIONE_8/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const hbs = require('hbs')
+const articoli = require('./articoli')
 
 
 const app = express()
@@ -8,6 +9,12 @@ const PORT = 4000
 app.set('view engine', 'hbs')
 hbs.registerPartials(__dirname + '/views/partials');
 app.use(express.static('./public'))
+
+let myPreferences = {
+    pet: "gatti",
+    color: "viola",
+    season: "autunno",
+}
 
 app.get('/' , (req, res) =>{
     res.render('home' ,{
@@ -20,15 +27,19 @@ app.get('/' , (req, res) =>{
 
 app.get('/about' , (req, res) =>{
     res.render('about', {
-        titolo: "Corso di Node.js",
+        titolo: "Corso su Node.js",
         colore: "arancio"
     })
 })
 
+app.get('/login' , (req, res) =>{
+    res.render('login', {myPreferences, titolo: "Login"} )
+})
 
 
-
-
+app.get('/lista', (req, res) =>{
+    res.render('lista' , {titolo: "Lista", data: articoli} )
+})
 
 
 
